84d0b1b8d0da5b442a9c4647e146964320702882
KCooperA <281289267@qq.com>
2019-11-29 21:40:14 +0800

    删除乱码

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_cors import CORS
 from mainapp import app
 from mainapp.views import function_api,user_api,name_test_api,gift_api,change_password_api,attention_api,intereted_api
 
-<<<<<<< HEAD
+
 def make_app():
     app.register_blueprint(function_api.user_function_blue, url_prefix='/api')
     app.register_blueprint(user_api.user_blue, url_prefix='/api/user')
@@ -11,17 +11,8 @@ def make_app():
     app.register_blueprint(change_password_api.change_blue,url_prefix='/api/change')
     app.register_blueprint(attention_api.my_blue, url_prefix='/api/Attention/')
     app.register_blueprint(intereted_api.feel_blue,url_prefix='/api/feel')
-=======
-if __name__ == '__main__':
-    app.register_blueprint(function_api.user_function_blue, url_prefix='/function')
-    app.register_blueprint(user_api.user_blue, url_prefix='/user')
-    app.register_blueprint(name_test_api.test_blue,url_prefix='/name')
-    app.register_blueprint(gift_api.gift_blue,url_prefix='/live')
-    app.register_blueprint(change_password_api.change_blue,url_prefix='/change')
-    app.register_blueprint(attention_api.my_blue, url_prefix='api/Attention/')
->>>>>>> 038af2c964d172607c126fdb66cbe5c4ebe2a32c
-    CORS(app)
 
+    CORS(app)
     return app
 
 application = make_app()
